d0292a8288c8e966b2519a3973d6773a54232e74
Tushar <tushar.abc15@gmail.com>
2026-02-12 19:38:24 +0000

    Update base path to match renamed repository

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/valentines-message/',
+  base: '/message-for-kanika/',
   plugins: [react()],
   build: {
     minify: 'terser',
